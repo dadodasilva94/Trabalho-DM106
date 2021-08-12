84331a153e35833185bb5db9ff1f3c2083932086
Paulo Eduardo <dado@pdsolucoes.com.br>
2021-08-12 20:07:09 -0300

    feat: Criando Model OrdemItem

---
+++ Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trabalho_DM106.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int amount { get; set; }
+
+        public int productId { get; set; }
+
+        public int orderId { get; set; }
+
+        public Product productInfo { get; set; }
+    }
+}
